17558388f7d2ad7dcae3563d92626f931696cca1
Marco Steiner <marco.steiner@swisscom.com>
2024-08-24 14:09:43 +0200

    extend regex and swap files

--- code/backend/internal/triploader/triploader.go
+++ code/backend/internal/triploader/triploader.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Loadtrips(dbpath string, stopId string, dayInYear int) (*[]models.Trip, error) {
-	var re = regexp.MustCompile(`(?m)^(\d+)\s+([A-Za-zäöüÄÖÜß\s-]+?)\s+(-?\d{5})\s+(-?\d{5})?\s+%`)
+	var re = regexp.MustCompile(`(?m)^(\d+)\s+([A-Za-zäöüÄÖÔÜÙÛÁÀËÉÈÙÂÎÏŸŒßùûòôâáàéèëœîïÿ,.()/\s-]+?)\s+(-?\d{5})\s+(-?\d{5})?\s+%`)
 
 	searchDB, err := sql.Open("sqlite3", dbpath)
 	var trips []models.Trip
