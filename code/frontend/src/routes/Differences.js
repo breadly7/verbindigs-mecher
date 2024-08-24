0d9a175239ffbd942feba805594c1fb27291367f
Lara Bärtschi <info@lara.ninja>
2024-08-24 16:59:49 +0200

    feat(frontend): add react-select

--- code/frontend/src/routes/Differences.js
+++ code/frontend/src/routes/Differences.js
@@ -48,7 +48,7 @@ const Differences = () => {
     };
 
     return (
-        <PageTemplate title="Differences">
+        <PageTemplate title="Differences (broken connections)">
             {loading ? (
                 <div className="flex justify-center items-center h-64">
                     <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
